ac5c782855d51dd925423244469178e124fc0594
Omar Moreno <email@omarmoreno.net>
2021-09-27 16:19:08 -0700

    First attemp to fill ROC subpacket.

--- software/emulators/include/emulators/HCalGenerator.h
+++ software/emulators/include/emulators/HCalGenerator.h
@@ -9,12 +9,13 @@ class HCalGenerator : public Generator {
 
 public:
   static std::shared_ptr<HCalGenerator> create() {
-    static std::shared_ptr<HCalGenerator> ret = std::make_shared<HCalGenerator>();
+    static std::shared_ptr<HCalGenerator> ret =
+        std::make_shared<HCalGenerator>();
     return (ret);
   }
 
   /// Constructor
-  HCalGenerator() : Generator() {};
+  HCalGenerator() : Generator(){};
 
   /// Default Constructor
   ~HCalGenerator() = default;
@@ -27,28 +28,28 @@ public:
   void genFrame(uint32_t size);
 
   /** @return the TX count */
-  uint32_t getTxCount() {};
+  uint32_t getTxCount(){};
 
   /** @return the total TX bytes */
-  uint32_t getTxBytes() {};
+  uint32_t getTxBytes(){};
 
   /** @return the total TX errors */
-  uint32_t getTxErrors() {};
+  uint32_t getTxErrors(){};
 
 private:
   /**
    */
-  std::vector<uint32_t> buildRocSubpackets(int n_links); 
-
+  std::vector<uint32_t> buildRocSubpackets(int n_links, uint16_t orbit_counter,
+                                           uint16_t bunch_id);
 
   /// Number of subpackets from the ROCs (NLINKS)
   uint8_t n_links_{0x1};
 
- /// NLINKS mask
- uint8_t n_links_mask_{0xE};
+  /// NLINKS mask
+  uint8_t n_links_mask_{0xE};
 
   /// FPGA ID
-  uint8_t fpga_id_{0x2}; 
+  uint8_t fpga_id_{0x2};
 
   /// FPGA ID Mask
   uint8_t fpga_id_mask_{0x14};
@@ -57,20 +58,50 @@ private:
   uint8_t format_version_{0x1};
 
   /// Format version mask
-  uint8_t format_version_mask_{0x1C}; 
+  uint8_t format_version_mask_{0x1C};
 
- /// Orbit counter
- uint16_t orbit_counter_{0}; 
+  /// Orbit counter
+  uint16_t orbit_counter_{0};
 
- /// Readout request mask
- uint8_t rr_mask_{0xA}; 
+  /// Readout request mask
+  uint8_t rr_mask_{0xA};
 
- /// The bunch ID
- uint16_t bunch_id_{0}; 
+  /// The bunch ID
+  uint16_t bunch_id_{0};
 
- /// Orbit counter mask
- uint8_t bunch_id_mask_{0x14}; 
+  /// Orbit counter mask
+  uint8_t bunch_id_mask_{0x14};
 
+  //
+  // ROC Subpacket
+  //
+
+  /// CRC OK mask
+  uint8_t crc_ok_mask_{0xF};
+
+  /// ROC ID mask
+  uint8_t roc_id_mask_{0x10};
+
+  /// Hamming errors mask
+  uint8_t hamming_errors_mask_{0x5};
+
+  /// Orbit counter mask
+  uint8_t roc_orbit_counter_mask_{0x7};
+
+  /// ROC readout request mask
+  uint8_t roc_rr_mask_{0xA};
+
+  /// Orbit counter mask
+  uint8_t roc_bunch_id_mask_{0x10};
+
+  /// ADC mask
+  uint8_t adc_mask_{0xA}; 
+ 
+  /// ADC t-1 mask
+  uint8_t adc_t1_mask_{0x14}; 
+
+  /// Channel is valid mask
+  uint8_t channel_valid_mask_{0x1E}; 
 };
 
 } // namespace emulators
--- software/emulators/src/emulators/HCalGenerator.cxx
+++ software/emulators/src/emulators/HCalGenerator.cxx
@@ -2,9 +2,9 @@
 
 #include <iostream>
 
-#include <bitset>
 #include "rogue/interfaces/stream/Frame.h"
-#include "rogue/interfaces/stream/FrameIterator.h" 
+#include "rogue/interfaces/stream/FrameIterator.h"
+#include <bitset>
 
 namespace emulators {
 
@@ -12,58 +12,98 @@ void HCalGenerator::genFrame(uint32_t size) {
   rogue::interfaces::stream::FramePtr frame;
   rogue::interfaces::stream::FrameIterator it;
 
-  frame = reqFrame(8, true); 
+  frame = reqFrame(8, true);
   frame->setPayload(8);
 
-
   // TODO: fix this calc
-  uint16_t len = 4 + n_links_*10;
-  uint32_t header{len}; 
+  uint16_t len = 4 + n_links_ * 10;
+  uint32_t header{len};
   header |= (n_links_ << n_links_mask_);
   header |= (fpga_id_ << fpga_id_mask_);
   header |= (format_version_ << format_version_mask_);
 
-  std::cout << "len: " << len << std::endl; 
-  std::cout << "[ HCalGenerator ]: Header [31:0]: " 
-	        << std::bitset<32>(header) << std::endl;   
+  std::cout << "len: " << len << std::endl;
+  std::cout << "[ HCalGenerator ]: Header [31:0]: " << std::bitset<32>(header)
+            << std::endl;
 
   toFrame(it, 4, &header);
 
-  header = orbit_counter_; 
-  header |= (0x1 << rr_mask_); 
-  header |= (bunch_id_ << bunch_id_mask_);   
+  header = orbit_counter_;
+  header |= (0x1 << rr_mask_);
+  header |= (bunch_id_ << bunch_id_mask_);
 
-  std::cout << "[ HCalGenerator ]: Header [63:32]: " 
-	        << std::bitset<32>(header) << std::endl;   
+  std::cout << "[ HCalGenerator ]: Header [63:32]: " << std::bitset<32>(header)
+            << std::endl;
 
   toFrame(it, 4, &header);
-
-  // Increment the bunch ID
-  bunch_id_ += 1; 
-
-  // Assume 10 bunches per train
-  if ((bunch_id_ + 1)%10 == 0) orbit_counter_ += 1; 
 
   // Build the ROC subpackets for each link
-  std::vector<uint32_t> subpackets{buildRocSubpackets(int(n_links_))}; 
+  std::vector<uint32_t> subpackets{buildRocSubpackets(int(n_links_), orbit_counter_, bunch_id_)};
+  int j{0};
   for (auto &subpacket : subpackets) {
-    toFrame(it, 4, &subpacket); 
+    std::cout << "[ HCalGenerator ]: ROC subpacket j = " << j << " : "
+              << std::bitset<32>(subpacket) << std::endl;
+    toFrame(it, 4, &subpacket);
+    ++j;
   }
 
+  // Increment the bunch ID
+  bunch_id_ += 1;
+
+  // Assume 10 bunches per train
+  if ((bunch_id_ + 1) % 10 == 0)
+    orbit_counter_ += 1;
+
   // The CRC-32 tail -- Not currently used
-  uint32_t tail{0}; 
-  std::cout << "[ HCalGenerator ]: Tail: " 
-	        << std::bitset<32>(tail) << std::endl;   
-  toFrame(it, 4, &tail); 
+  uint32_t tail{0};
+  std::cout << "[ HCalGenerator ]: Tail: " << std::bitset<32>(tail)
+            << std::endl;
+  toFrame(it, 4, &tail);
 
-  sendFrame(frame);  
-
+  sendFrame(frame);
 }
 
-std::vector<uint32_t> HCalGenerator::buildRocSubpackets(int n_links) {
-  std::vector<uint32_t> packet{int(n_links*42), 0x0};
+std::vector<uint32_t> HCalGenerator::buildRocSubpackets(int n_links,
+                                                        uint16_t orbit_counter,
+                                                        uint16_t bunch_id) {
+  std::vector<uint32_t> packet(int(n_links * 42), 0x0);
+  std::cout << "packet size: " << packet.size() << std::endl;
 
-  return packet;  
+  // Build the header of the subpacket
+  packet[0] = 0xFF; // Readout map
+  packet[0] |= (0x1 << crc_ok_mask_);
+  packet[0] |= (0x1 << roc_id_mask_);
+
+  // Set the rest of the readout map to all 1's
+  packet[1] = -1;
+
+  packet[2] = 0x5;
+  packet[2] |= (0x0 << hamming_errors_mask_);
+  packet[2] |= (orbit_counter << roc_orbit_counter_mask_);
+  packet[2] |= (0x1 << roc_rr_mask_);
+  packet[2] |= (bunch_id << roc_bunch_id_mask_);
+  packet[2] |= (0x5 << 0x1C);
+
+  packet[3] = 0x3FF; 
+  packet[3] |= (0x0 << adc_mask_); 
+  packet[3] |= (0x0 << adc_t1_mask_); 
+  packet[3] |= (0x2 << channel_valid_mask_);
+
+  uint16_t adc_t1{100}; 
+  uint16_t adc{200}; 
+  uint16_t toa{1}; 
+  for (int i{4}; i < packet.size() - 1; ++i) {
+    packet[i] = toa; 
+    packet[i] |= (adc << adc_mask_); 
+    packet[i] |= (adc_t1 << adc_t1_mask_); 
+    packet[i] |= (0x0 << channel_valid_mask_); 
+  
+    adc_t1++; 
+    adc++; 
+    toa++; 
+  }
+
+  return packet;
 }
 
-}
+} // namespace emulators
