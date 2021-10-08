027f0abb45dae4887a9f51ca32f8a4f0f2f5b8da
GNiramay <niramaygogate@gmail.com>
2021-10-08 23:04:36 +0000

    Added a decoder to the Receiver

--- software/emulators/include/emulators/TrigScint_SlowControlReceiver.h
+++ software/emulators/include/emulators/TrigScint_SlowControlReceiver.h
@@ -23,7 +23,17 @@ public:
   /**
    */
   void acceptFrame(std::shared_ptr<rogue::interfaces::stream::Frame> frame);
-  
+
+  /**
+   * @note Print QIE Information from 72-bit data
+   */
+  void PrintQIEInfo(std::vector<uint8_t> word_);
+
+private:
+  float TDC_Threshold_LSB = 0.156;   // least possibble TDC threshold [uA]. Might also be 3.74
+  float LVDS_V_OUT[4] = {150, 250, 350, 450};
+  float Gsel[13] = {3.1,4.65,6.2,9.3,12.4,15.5,18.6,21.7,24.8,27.9,31,34.1,35.65};
+
 };
 }
 
