e9f08683cd18e28454c5bcf705bbec48ccb8b3ec
Omar Moreno <email@omarmoreno.net>
2021-10-14 00:20:44 -0700

    Add option to specify the sender. Allow file path and name to be set.

--- software/eudaq/src/eudaq/RogueTcpClientProducer.cxx
+++ software/eudaq/src/eudaq/RogueTcpClientProducer.cxx
@@ -3,6 +3,9 @@
 //---< ldmx-rogue >---//
 #include "rogue/Commands.h"
 
+//---< ldmx-eudaq >---//
+#include "eudaq/HCalDataSender.h"
+
 namespace {
 auto dummy0 = eudaq::Factory<eudaq::Producer>::Register<
     eudaq::RogueTcpClientProducer, const std::string &, const std::string &>(
@@ -14,8 +17,6 @@ namespace eudaq {
 RogueTcpClientProducer::RogueTcpClientProducer(const std::string &name,
                                                const std::string &runcontrol)
     : eudaq::Producer(name, runcontrol) {
-
-  sender_ = RogueDataSender::create(this);
 } 
 
 void RogueTcpClientProducer::DoInitialise() {
@@ -39,25 +40,40 @@ void RogueTcpClientProducer::DoInitialise() {
   // Connect the tcp stream to the file writer
   tcp_->addSlave(writer_->getChannel(0)); 
 
-  // Get the path to the output file
-  output_path_ = ini->Get("OUTPUT_PATH", ".");
-
-  // Get the file prefix
-  file_prefix_ = ini->Get("FILE_PREFIX", "ldmx_test"); 
+  // Configure the sender
+  // TODO: Dynamically load these instead
+  auto sender_type{ini->Get("SENDER", "HCal")}; 
+  if (sender_type.compare("HCal") == 0) sender_ = HCalDataSender::create(this); 
+  else {
+    EUDAQ_THROW("Failed to specify SENDER");
+  }
+  EUDAQ_INFO("Using a data sender of type " + sender_type); 
 
   // Connect the data sender to the TCP client
   tcp_->addSlave(sender_); 
 }
 
 void RogueTcpClientProducer::DoConfigure() {
-  
+
+  // Get the configuration
+  auto conf{GetConfiguration()};
+
+  // Get the path to the output file
+  output_path_ = conf->Get("OUTPUT_PATH", ".");
+
+  // Get the file prefix
+  file_prefix_ = conf->Get("FILE_PREFIX", "ldmx_test"); 
+
+  // Build the file name
+  auto output_file{output_path_ + "/" + file_prefix_ + "_" + std::to_string(GetRunNumber()) + ".dat"}; 
+
   // First, make sure an existing file isn't open.
   if (writer_->isOpen()) writer_->close(); 
 
   // Open a file to write the stream
-  // TODO: Append the run number to this?
-  writer_->open(output_path_ + "/" + file_prefix_ + ".dat"); 
+  writer_->open(output_file);
 
+  EUDAQ_INFO("Writing rogue stream to " + output_file);
 }
 
 void RogueTcpClientProducer::DoStartRun() {
@@ -76,7 +92,6 @@ void RogueTcpClientProducer::DoReset() {}
 void RogueTcpClientProducer::DoTerminate() {}
 
 void RogueTcpClientProducer::RunLoop() {
-  //auto event{eudaq::Event::MakeUnique("LDMXTestBeamRaw")};
 }
 
 } // namespace eudaq
