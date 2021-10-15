90058d43c1114f98fb987fde74cef33ca18001f3
Omar Moreno <email@omarmoreno.net>
2021-10-15 11:36:42 -0700

    Clean up to prep for merge.

--- software/emulators/src/emulators/TrigScintGenerator.cxx
+++ software/emulators/src/emulators/TrigScintGenerator.cxx
@@ -1,14 +1,17 @@
 #include "emulators/TrigScintGenerator.h"
 
+//---< StdLib >---//
+#include <bitset>
 #include <iostream>
 #include <math.h>
+#include <stdlib.h>
 
+//---< rogue >---//
 #include "rogue/interfaces/stream/Frame.h"
 #include "rogue/interfaces/stream/FrameIterator.h"
-#include <bitset>
 
-#include "emulators/QIE_DataPacket.h"
-#include <stdlib.h>     /* srand, rand */
+//---< emulators >---//
+#include "emulators/QieDataPacket.h"
 
 namespace emulators {
 
@@ -17,7 +20,7 @@ void TrigScintGenerator::genFrame(uint32_t size) {
   rogue::interfaces::stream::FrameIterator it;
 
   // Calculate the total size of the frame
-  uint16_t len = 124;		// 31*32/8
+  uint16_t len = 124; // 31*32/8
 
   // Request a frame of the size above and get an iterator to the
   // beginning of the frame.
@@ -26,29 +29,30 @@ void TrigScintGenerator::genFrame(uint32_t size) {
   it = frame->begin();
 
   // Initialize QIE DataPacket
-  QIE_DataPacket* qie = new QIE_DataPacket(event_number_);
+  auto qie{new QieDataPacket(event_number_)};
 
   // Set flags
-  qie->SetFlags(false,false,false,false);
+  qie->setFlags(false, false, false, false);
 
   // Fill adc, tdc data (randomly)
-  srand (time(NULL));
-  for(int id=0;id<12;id++){
-    for(int ts=0;ts<5;ts++){
-      int adc = rand() % 256;
-      int tdc = rand() % 50;
-      qie->AddQIEData(id,ts,adc,tdc);
+  srand(time(NULL));
+  for (int id{0}; id < 12; ++id) {
+    for (int ts{0}; ts < 5; ++ts) {
+      int adc{rand() % 256};
+      int tdc{rand() % 50};
+      qie->addQieData(id, ts, adc, tdc);
     }
   }
 
   // Generate data packet
-  auto data = qie->FormPacket();
+  auto data = qie->formPacket();
 
-  for(int i=0;i<len;i++){
-    toFrame(it,1,&data[i]);
+  for (int i = 0; i < len; i++) {
+    toFrame(it, 1, &data[i]);
   }
   ++event_number_;
-  qie->PrintQIEInfo(data);
+ 
+  std::cout << qie << std::endl;
 
   // Build the QIE subpackets for each link
   sendFrame(frame);
--- software/emulators/src/emulators/TrigScintReceiver.cxx
+++ software/emulators/src/emulators/TrigScintReceiver.cxx
@@ -4,60 +4,59 @@
 #include <bitset>
 #include <iostream>
 
+//---< rogue >---//
 #include "rogue/interfaces/stream/Frame.h"
 #include "rogue/interfaces/stream/FrameIterator.h"
 
-#include "emulators/QIE_DataPacket.h"
+//---< emulators >---//
+#include "emulators/QieDataPacket.h"
 
 namespace emulators {
 
-  void TrigScintReceiver::acceptFrame(
-  				      std::shared_ptr<rogue::interfaces::stream::Frame> frame) {
+void TrigScintReceiver::acceptFrame(
+    std::shared_ptr<rogue::interfaces::stream::Frame> frame) {
 
-    // Get an iterator to the data in the frame
-    auto it{frame->begin()};
+  // Get an iterator to the data in the frame
+  auto it{frame->begin()};
 
-    // Check for errors. If errors are found, skip processing of the frame.
-    if (frame->getError()) {
-      ++rx_error_count_;
-      return;
-    }
-
-    // Get a placeholder to read the frame
-    uint8_t word;
-    int n_ts = 5;
-    int n_qie = 12;
-    
-    rogue::interfaces::stream::fromFrame(it, 1, &word);
-    int trig_id = 255*word;
-    rogue::interfaces::stream::fromFrame(it, 1, &word);
-    trig_id += word;
-    QIE_DataPacket* packet = new QIE_DataPacket(trig_id);
-    
-    rogue::interfaces::stream::fromFrame(it, 1, &word);
-    packet->SetFlags(
-		  ((word>>3)&1),
-		  ((word>>2)&1),
-		  ((word>>1)&1),
-		  (word&1));
-
-    rogue::interfaces::stream::fromFrame(it, 1, &word);
-    for(int ts=0;ts<n_ts;ts++) {
-      for(int qie=0;qie<n_qie;qie++) {
-	rogue::interfaces::stream::fromFrame(it, 1, &word);
-	packet->adc[ts][qie]=word;
-      }
-      
-      for(int qie=0;qie<n_qie;qie++) {
-	rogue::interfaces::stream::fromFrame(it, 1, &word);
-	packet->tdc[ts][qie]=word;
-      }
-    }
-
-    packet->PrintQIEInfo(packet->FormPacket());
-    
-    ++rx_count_;
-    rx_bytes_ += frame->getPayload();
+  // Check for errors. If errors are found, skip processing of the frame.
+  if (frame->getError()) {
+    ++rx_error_count_;
+    return;
   }
+
+  // Get a placeholder to read the frame
+  uint8_t word;
+  int n_ts{5};
+  int n_qie{12};
+
+  rogue::interfaces::stream::fromFrame(it, 1, &word);
+  int trig_id = 255 * word;
+  rogue::interfaces::stream::fromFrame(it, 1, &word);
+  trig_id += word;
+  auto packet{new QieDataPacket(trig_id)};
+
+  rogue::interfaces::stream::fromFrame(it, 1, &word);
+  packet->setFlags(((word >> 3) & 1), ((word >> 2) & 1), ((word >> 1) & 1),
+                   (word & 1));
+
+  rogue::interfaces::stream::fromFrame(it, 1, &word);
+  for (int ts = 0; ts < n_ts; ts++) {
+    for (int qie = 0; qie < n_qie; qie++) {
+      rogue::interfaces::stream::fromFrame(it, 1, &word);
+      packet->setADC(ts, qie, word);
+    }
+
+    for (int qie = 0; qie < n_qie; qie++) {
+      rogue::interfaces::stream::fromFrame(it, 1, &word);
+      packet->setTDC(ts, qie, word);
+    }
+  }
+
+  std::cout << packet << std::endl;
+
+  ++rx_count_;
+  rx_bytes_ += frame->getPayload();
+}
 
 }; // namespace emulators
