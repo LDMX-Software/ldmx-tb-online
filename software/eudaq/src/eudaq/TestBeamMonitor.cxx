ba76962700fce8175470d10ce2a0c216cad24c42
Omar Moreno <email@omarmoreno.net>
2021-10-21 15:56:06 -0700

    Update monitoring to handle multi-samples.

--- software/eudaq/src/eudaq/TestBeamMonitor.cxx
+++ software/eudaq/src/eudaq/TestBeamMonitor.cxx
@@ -15,10 +15,10 @@ namespace eudaq {
 void TestBeamMonitor::AtConfiguration() {
   // TODO Use DAQ map to fill map
   for (int i{256}; i < 264; ++i) {
-    histo_map["ROC " + std::to_string(i) + " - ADC"] = m_monitor->Book<TH2D>(
-        "ROC " + std::to_string(i) + " ADC", "ROC_" + std::to_string(i) + "_ADC", 
-	"", ";Channel;ADC",
-        40, 0, 40, 200, 0, 200);
+    histo_map["ROC " + std::to_string(i) + " - ADC"] =
+        m_monitor->Book<TH2D>("ROC " + std::to_string(i) + " ADC",
+                              "ROC_" + std::to_string(i) + "_ADC", "",
+                              ";Channel;ADC", 40, 0, 40, 200, 0, 200);
     m_monitor->SetDrawOptions(histo_map["ROC " + std::to_string(i) + " - ADC"],
                               "colz");
   }
@@ -28,12 +28,14 @@ void TestBeamMonitor::AtEventReception(EventSP event) {
   // Fill HCal plots
   auto hcal_event{std::make_shared<HgcrocDataPacket>(*event)};
 
-  auto subpackets{hcal_event->getSubpackets()};
-  for (auto &subpacket : subpackets) {
-    auto roc_id{subpacket.roc_id};
-    for (int i{0}; i < subpacket.adc.size(); ++i) {
-      histo_map["ROC " + std::to_string(roc_id) + " - ADC"]->Fill(
-          i, subpacket.adc[i]);
+  auto samples{hcal_event->getSamples()};
+  for (auto &sample : samples) {
+    for (auto &subpacket : sample.subpackets) {
+      auto roc_id{subpacket.roc_id};
+      for (int i{0}; i < subpacket.adc.size(); ++i) {
+        histo_map["ROC " + std::to_string(roc_id) + " - ADC"]->Fill(
+            i, subpacket.adc[i]);
+      }
     }
   }
   // if (event->GetDescription().compare("HCalTestBeamRaw") == 0) {
