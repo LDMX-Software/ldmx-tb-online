a1c8f771db22e991ad9496a6abf01da3ead5c423
pbutti <pierfrancesco.butti@gmail.com>
2021-10-24 11:44:36 +0200

    some cleanup

--- software/eudaq/src/eudaq/TestBeamMonitor.cxx
+++ software/eudaq/src/eudaq/TestBeamMonitor.cxx
@@ -42,51 +42,63 @@ void TestBeamMonitor::AtConfiguration() {
         hcal_event_map[i] = m_monitor->Book<TH2D>("Event/Plane " + std::to_string(i), "Plane " + std::to_string(i), "", ";Bar End;Channel", 2, 0, 2, 24, 0, 24);
     }
     m_monitor->SetDrawOptions(hcal_run_map[i], "colz");
-    m_monitor->SetDrawOptions(hcal_event_map[i], "colz");
   }
   }
   ts_run = m_monitor->Book<TH2D>("Run/TrigScint", "TrigScint", "", ";Channel; ", 1, 0, 1, 12, 0, 12);
-  ts_event = m_monitor->Book<TH2D>("Event/TrigScint", "TrigScint", "", ";Channel; ", 1, 0, 1, 12, 0, 12);
   m_monitor->SetDrawOptions(ts_run, "colz");
-  m_monitor->SetDrawOptions(ts_event, "colz");
   daq_map = getDaqMap("/home/ldmx/OnlineMonitoring_test/hcal_map.csv");
 }
 
 void TestBeamMonitor::AtEventReception(EventSP event) {
 
-  bool newSpill = false; //TODO Set new spill flag 
-  //Reset event plots if new spill
-  if(newSpill){
-    for (int i{1}; i < nPlanes + 1; i++){
-      //hcal_event_map[i].Reset();
-    }
-    //ts_event.Reset();
-  }
   // Fill HCal plots
   auto hcal_event{std::make_shared<HgcrocDataPacket>(*event)};
-
-  auto subpackets{hcal_event->getSubpackets()};
-  for (auto &subpacket : subpackets) {
-    auto roc_id{subpacket.roc_id};
-    std::cout << "Samples: " << subpacket.adc.size() << std::endl;
-    for (int i{0}; i < subpacket.adc.size(); ++i) {
-      std::cout << "Roc ID: " << std::to_string(roc_id) << "  ADC " << subpacket.adc[i] << std::endl;
-      histo_map["ROC " + std::to_string(roc_id) + " - ADC"]->Fill(
-          i, subpacket.adc[i]);
-      if(subpacket.adc[i] >= hcal_adc_thresh){
-          std::string channel = std::to_string(roc_id) + "," + std::to_string(i);
-          int plane = getPlane(daq_map[channel]);
-          int cmb = getCMB(daq_map[channel]);
-          int quadbar = getQuadBar(daq_map[channel]);
-          int bar = getBar(daq_map[channel]);
-          int end = cmb%2;
-          int chan = (bar - 1) + (quadbar - 1) * 4;
+  
+  auto samples{hcal_event->getSamples()};
+  
+  auto thresh_map{std::map<std::string, int>()};
+  
+  for (auto &sample : samples) {
+    for (auto &subpacket : sample.subpackets) {
+      auto roc_id{subpacket.roc_id};
+      std::cout << "Samples: " << subpacket.adc.size() << std::endl;
+      for (int i{0}; i < subpacket.adc.size(); ++i) {
+        histo_map["ROC " + std::to_string(roc_id) + " - ADC"]->Fill(
+            i, subpacket.adc[i]);
+        if(i >= 32){
+          continue;
+        }
+        int roc_num = (int) (round((roc_id - 256) / 2) + 1);
+        int chan_num = i + 32 * (roc_id%2);
+        std::string channel = std::to_string(roc_num) + "," + std::to_string(chan_num);
+        if(!daq_map.count(channel)){
+          std::cout<<"No map found " << channel << std::endl;
+          continue;
+        }
+        int plane = getPlane(daq_map[channel]);
+        if(!hcal_run_map.count(plane) || !hcal_event_map.count(plane)){
+          std::cout<< "Histo for plane " << std::to_string(plane) << " not found" << std::endl;
+          continue;
+        }
+        int cmb = getCMB(daq_map[channel]);
+        int quadbar = getQuadBar(daq_map[channel]);
+        int bar = getBar(daq_map[channel]);
+        int end = cmb%2;
+        int chan = (quadbar - 1) * 4 + (4 - bar) * end + (1 - bar) * (end - 1);
+        std::string pedchan = "HcalDigiID(0:" + std::to_string(plane) + ":" + std::to_string((4 - bar) + (quadbar - 1) * 4) + ":" + std::to_string(end) + ")";
+        if(!ped_map.count(pedchan)){
+          std::cout<< "Threshold/Pedestal Map for " << pedchan << " not found" << std::endl;
+          continue;
+        }
+        int pedestal = getPedest(ped_map[pedchan]);
+        int threshold = getThresh(ped_map[pedchan]);
+        int thresh = pedestal + threshold;
+        if(subpacket.adc[i] >= thresh && !thresh_map.count(channel)){
+          thresh_map.insert(std::pair<std::string, int>(channel, 1));
           if(plane%2 != 0){
-            hcal_run_map[plane]->Fill(chan, end);
-            hcal_event_map[plane]->Fill(chan, end);
+            hcal_run_map[plane]->Fill(chan, 1 - end);
           } else{
             hcal_run_map[plane]->Fill(end, chan);
-            hcal_event_map[plane]->Fill(end, chan);
           }
       }
     }
