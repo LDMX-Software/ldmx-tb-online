c9f1fe5949bfc53aaba21fac85ec1630493b1d80
Jeremy McCormick <jeremym@slac.stanford.edu>
2021-11-13 01:18:49 -0600

    Add command line interface

--- examples/dash_test.py
+++ examples/dash_test.py
@@ -1,42 +1,74 @@
-import plotly.express as px
+import argparse
+import rogue
+from dash.dependencies import Input, Output
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.express as px
 import pandas as pd
-from dash.dependencies import Input, Output
+pd.options.mode.chained_assignment = None
 
-import rogue
+parser = argparse.ArgumentParser()
+parser.add_argument('-a', '--addr', help="IP address or hostname of server", default="127.0.0.1")
+parser.add_argument('-p', '--port', type=int, help="Port of server", default=8000)
+parser.add_argument('-n', '--npoints', type=int, help="Number of data points to display", default=25)
+parser.add_argument('-i', '--interval', type=int, help="Update interval in seconds", default=1)
+parser.add_argument('-d', '--debug', action='store_true',  help="Turn on debugging output", default=False)
+cl = parser.parse_args()
+
+addr = cl.addr
+port = cl.port
+npoints = cl.npoints
+update_interval = cl.interval
+debug = cl.debug
 
 df = pd.DataFrame(columns=["BytesRecd", "FramesRecd"])
-
-client = rogue.interfaces.stream.TcpClient("127.0.0.1", 8000)
+client = rogue.interfaces.stream.TcpClient(addr, port)
 slave = rogue.interfaces.stream.Slave()
 client._addSlave(slave)
 
+counter_text_style = {'display': 'inline-block',
+                      'font-size': 30,
+                      'font-family': ['Roboto Mono', 'Arial'],
+                      'border': '1px solid',
+                      'vertical-align': 'top',
+                      'padding': '5px',
+                      'margin-top': '30px'}
+
 app = dash.Dash()
 app.layout = html.Div([
-    html.Div(id='total-bytes'),
-    dcc.Graph(id='bytes-graph'),
-    html.Div(id='total-frames'),
-    dcc.Graph(id='frames-graph'),
+    html.H1(children="{}:{}".format(addr, port), style={'text-align': 'center'}),
+    html.Div(id='bytes-counter',
+             children="0",
+             style=counter_text_style),
+    dcc.Graph(id='bytes-graph',
+              style={'display': 'inline-block'}),
+    html.Div(id='frames-counter',
+             children="0",
+             style=counter_text_style),
+    dcc.Graph(id='frames-graph',
+              style={'display': 'inline-block'}),
     dcc.Interval(id='interval-component',
-        interval=1*1000,
-        n_intervals=0)
+                 interval=update_interval*1000,
+                 n_intervals=0)
 ])
 
-@app.callback(Output('bytes-graph', 'figure'),
+
+@app.callback(Output('bytes-counter', 'children'),
+              Output('bytes-graph', 'figure'),
+              Output('frames-counter', 'children'),
               Output('frames-graph', 'figure'),
-              Output('total-bytes', 'children'),
-              Output('total-frames', 'children'),
               Input('interval-component', 'n_intervals'))
 def update_graph_live(n):
-    global df
     bc = slave.getByteCount()
     fc = slave.getFrameCount()
     df.loc[len(df.index)] = [bc, fc]
-    df = df.tail(50)
-    fig1 = px.line(df, x=df.index, y=df['BytesRecd'].diff(), title='Bytes Received')
-    fig2 = px.line(df, x=df.index, y=df['FramesRecd'].diff(), title='Frames Received')
-    return fig1, fig2, bc, fc
+    dftail = df.tail(npoints)
+    fig1 = px.line(df, x=dftail.index, y=dftail['BytesRecd'].diff(), title='Bytes Received')
+    fig1.update_layout(xaxis_title='Seconds', yaxis_title='Bytes', font=dict(size=18))
+    fig2 = px.line(df, x=dftail.index, y=dftail['FramesRecd'].diff(), title='Frames Received')
+    fig2.update_layout(xaxis_title='Seconds', yaxis_title='Frames', font=dict(size=18))
+    return bc, fig1, fc, fig2
 
-app.run_server(debug=True) 
+
+app.run_server(debug=debug)
