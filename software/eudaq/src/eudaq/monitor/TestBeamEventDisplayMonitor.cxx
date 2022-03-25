0984a1abe0f3975c8dfa35e999fc42f379047f71
mrsolt <mrsolt@alumni.stanford.edu>
2022-03-25 18:20:47 +0000

    cleanup event display

--- software/eudaq/src/eudaq/monitor/TestBeamEventDisplayMonitor.cxx
+++ software/eudaq/src/eudaq/monitor/TestBeamEventDisplayMonitor.cxx
@@ -4,6 +4,8 @@
 
 #include "eudaq/HgcrocDataPacket.h"
 
+#include "eudaq/CSVParser.h"
+
 namespace {
 auto dummy0 = eudaq::Factory<eudaq::Monitor>::Register<
     eudaq::TestBeamEventDisplayMonitor, const std::string &, const std::string &>(
@@ -35,8 +37,17 @@ void TestBeamEventDisplayMonitor::AtConfiguration() {
   }
   ts_event = m_monitor->Book<TH2D>("Event/TrigScint", "TrigScint", "", ";Channel; ", 1, 0, 1, 12, 0, 12);
   m_monitor->SetDrawOptions(ts_event, "colz");
-  daq_map = getDaqMap("/home/ldmx/OnlineMonitoring_test/hcal_map.csv");
-  ped_map = getPedMap("/home/ldmx/OnlineMonitoring_test/DumbReconConditions.csv");
+  std::string daqmapfile = "~/OnlineMonitor/ldmx-tb-online/software/data/testbeam_connections.csv";
+  std::string gainfile = "~/OnlineMonitor/ldmx-tb-online/software/data/testbeam_connections.csv";
+  cmb_map = CSVParser::getCMBMap(daqmapfile);
+  quadbar_map = CSVParser::getQuadbarMap(daqmapfile);
+  bar_map = CSVParser::getBarMap(daqmapfile);
+  plane_map = CSVParser::getPlaneMap(daqmapfile);
+  detid_map = CSVParser::getDetIDMap(gainfile);
+  adcped_map = CSVParser::getADCPedMap(gainfile);
+  adcgain_map = CSVParser::getADCGainMap(gainfile);
+  totped_map = CSVParser::getTOTPedMap(gainfile);
+  totgain_map = CSVParser::getTOTGainMap(gainfile);
 }
 
 void TestBeamEventDisplayMonitor::AtEventReception(EventSP event) {
@@ -69,36 +80,29 @@ void TestBeamEventDisplayMonitor::AtEventReception(EventSP event) {
       auto roc_id{subpacket.roc_id};
       std::cout << "Samples: " << subpacket.adc.size() << std::endl;
       for (int i{0}; i < subpacket.adc.size(); ++i) {
+        std::cout<<subpacket.roc_id<<"  "<<i<<"  "<<subpacket.adc[i]<<std::endl;
         if(i >= 32){
           continue;
         }
-        int roc_num = (int) (round((roc_id - 256) / 2) + 1);
-        int chan_num = i + 32 * (roc_id%2);
-        std::string channel = std::to_string(roc_num) + "," + std::to_string(chan_num);
-        if(!daq_map.count(channel)){
-          std::cout<<"No map found " << channel << std::endl;
-          continue;
-        }
-        int plane = getPlane(daq_map[channel]);
-        if(!hcal_event_map.count(plane)){
-          std::cout<< "Histo for plane " << std::to_string(plane) << " not found" << std::endl;
-          continue;
-        }
-        int cmb = getCMB(daq_map[channel]);
-        int quadbar = getQuadBar(daq_map[channel]);
-        int bar = getBar(daq_map[channel]);
-        int end = cmb%2;
-        int chan = (quadbar - 1) * 4 + (4 - bar) * end + (1 - bar) * (end - 1);
-        std::string pedchan = "HcalDigiID(0:" + std::to_string(plane) + ":" + std::to_string((4 - bar) + (quadbar - 1) * 4) + ":" + std::to_string(end) + ")";
-        if(!ped_map.count(pedchan)){
-          std::cout<< "Threshold/Pedestal Map for " << pedchan << " not found" << std::endl;
-          continue;
-        }
-        int pedestal = getPedest(ped_map[pedchan]);
-        int threshold = getThresh(ped_map[pedchan]);
-        int thresh = pedestal + threshold;
-        if(subpacket.adc[i] >= thresh && !thresh_map.count(channel)){
-          thresh_map.insert(std::pair<std::string, int>(channel, 1));
+
+	std::string rocchan = std::to_string(roc_id+1) + "," + std::to_string(i);
+	int cmb = cmb_map.at(rocchan);
+	int quadbar = quadbar_map.at(rocchan);
+	int bar = bar_map.at(rocchan);
+	int plane = plane_map.at(rocchan);
+	int end = cmb%2;
+	std::string digiid = "HcalDigiID(0:" + std::to_string(plane) + ":" + std::to_string((4 - bar) + (quadbar - 1) * 4) + ":" + std::to_string(end) + ")";
+	int chan = (quadbar - 1) * 4 + (4 - bar) * end + (1 - bar) * (end - 1);	
+	int detid = detid_map.at(digiid);
+	double adcped = adcped_map.at(digiid);
+	double adcgain = adcgain_map.at(digiid);
+	double totped = totped_map.at(digiid);
+	double totgain = totgain_map.at(digiid);
+
+	double thresh = adcped + adcgain;
+
+        if(subpacket.adc[i] >= thresh && !thresh_map.count(rocchan)){
+          thresh_map.insert(std::pair<std::string, int>(rocchan, 1));
           if(plane%2 != 0){
             hcal_event_map[plane]->Fill(chan, 1 - end);
           } else{
