78459a19789afc7fdeb9f30f4d563693b34ae3d7
mrsolt <mrsolt@alumni.stanford.edu>
2022-03-28 20:38:16 +0000

    adding sample file reader setup script

---
+++ software/setup_filereader.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+euRun -n DarkRunControl -a tcp://4001 &
+sleep 20
+
+euCliCollector -n TestBeamDataCollector -t test_beam -r tcp://localhost:4001 &
+
+sleep 1
+
+euCliProducer -n HgcrocFileReaderProducer -t hgcroc_file -r tcp://localhost:4001 &
+
+sleep 1
+
+#euCliMonitor -n TestBeamEventDisplayMonitor -t event_display -r tcp://localhost:4001 & 
+
+sleep 5
+
+euCliMonitor -n HCalTestBeamMonitor -t hcal_mon -r tcp://localhost:4001 &
