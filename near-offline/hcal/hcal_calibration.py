b96c7fe4ec5a8b1daa7e995efbb3bd587f0972f5
mrsolt <mrsolt@alumni.stanford.edu>
2022-04-04 02:05:39 +0000

    add gain calc to hcal calib script

--- near-offline/hcal/hcal_calibration.py
+++ near-offline/hcal/hcal_calibration.py
@@ -13,6 +13,11 @@ inputFile=TFile(sys.argv[1], "read") #input file from arguments
 outputFileName = 'DumbReconConditions.csv' #hardcoded name for now
 tree=inputFile.Get("ntuplizehgcroc/hgcroc") #get tree
 
+energy_per_mip = 4.66; #MeV/MIP
+voltage_hcal = 5.; #mV/PE
+PE_per_mip = 68.; #PEs/mip
+mV_per_PE = 1/energy_per_mip * voltage_hcal * PE_per_mip; #mV per MIP is about 73 for now
+
 header1 = ["# ElLoc Format: (rocid:channel)"]
 header2 = ["DetID", "ElLoc", "ADC_PEDESTAL", "ADC_GAIN", "TOT_PEDESTAL", "TOT_GAIN"]
 
@@ -37,6 +42,8 @@ with open(outputFileName, mode='w') as datafile:
             tot_ped = 1. #Dummy Value
             tot_gain = 2.5 #Dummy Value
             elloc = "{0}:{1}".format(roc, chan)
+            PE_chan = (maxadc - adcped) / mV_per_PE * adcgain; 
+            #adc_gain = mV_per_PE / (maxadc - adc_ped) #where maxadc is at single PE peak
             line = [str(raw_id), elloc, str(adc_ped), str(adc_gain), str(tot_ped), str(tot_gain)]
             writer.writerow(line)
             usedchans[raw_id] = True
