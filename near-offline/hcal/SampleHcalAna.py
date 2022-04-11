eb193ce5d8bca186872c1673811ccfb8a8530587
Matthew Solt <mrsolt@alumni.stanford.edu>
2022-04-11 11:54:25 +0200

    update sample hcal ana script

--- near-offline/hcal/SampleHcalAna.py
+++ near-offline/hcal/SampleHcalAna.py
@@ -17,6 +17,7 @@ c = TCanvas("c","c",800,600)
 f = ROOT.TFile(outputFileName,'recreate') #create root output file
 
 maxadc_histo = {} #histo map to channel
+alladc_histo = {}
 maxsample_histo = {}
 sumadc_histo = {}
 for e in tree : #Loop over events in tree
@@ -24,6 +25,7 @@ for e in tree : #Loop over events in tree
         for d in e.ChipSettingsTestDigis_unpack : #Loop over channels
             if d.id() not in maxadc_histo : #Create map key if not already there
                maxadc_histo[d.id()] = ROOT.TH1F(f'maxadc_eid_{d.id()}', f'Max ADC EID {d.id()}',1024,0,1024)
+               alladc_histo[d.id()] = ROOT.TH1F(f'alladc_eid_{d.id()}', f'All ADC EID {d.id()}',1024,0,1024)
                maxsample_histo[d.id()] = ROOT.TH1F(f'maxsample_eid_{d.id()}', f'Max Sample EID {d.id()}',8,0,8)
                sumadc_histo[d.id()] = ROOT.TH1F(f'sumadc_eid_{d.id()}', f'Sum ADC EID {d.id()}',1024,0,1024*4)
             maxadc = -9999.
@@ -31,6 +33,7 @@ for e in tree : #Loop over events in tree
             maxsample = -9999
             for i in range(d.size()) : #Loop over sample number
                sumadc = sumadc + d.at(i).adc_t()
+               alladc_histo[d.id()].Fill(d.at(i).adc_t())
                if(d.at(i).adc_t() > maxadc):
                    maxadc = d.at(i).adc_t()
                    maxsample = i
