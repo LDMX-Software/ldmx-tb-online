37a30159801be34c95fa4946b35d290eb6af1692
Matthew Solt <mrsolt@alumni.stanford.edu>
2022-04-19 10:34:12 +0200

    fix bug

--- software/eudaq/src/eudaq/monitor/HCalTestBeamMonitor.cxx
+++ software/eudaq/src/eudaq/monitor/HCalTestBeamMonitor.cxx
@@ -378,7 +378,7 @@ void HCalTestBeamMonitor::AtEventReception(EventSP event) {
         PEsum = physical_map.at(key0) + physical_map.at(key1);
       }
       if(newSpill){
-        if(j < 10){
+        if(i < 10){
           hcal_event->Fill(i, j+2, PEsum);
         }
         else{
