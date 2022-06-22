d1980b56c2dc6bb558de4f18d7ad1b28df6077e2
tomeichlersmith <eichl008@umn.edu>
2022-06-22 11:25:23 -0500

    introduce ability to skip some spills at start of PF file
    observing a aligned file that had an extremely low (<1%) alignment rate
    showed that the spill index was separated between the two PF files by 2
    due to two short spills only appearing at the beginning of the PF1 file
    
    skipping these spills offered a better alignment rate
    
    skipping implemented by extracting the unpacking of an event and
    decoding of the header into its own member function and then updating
    PolarfireRawFile::next to perform the logic on whether to keep/drop the
    current spill

--- reformat/TestBeam/src/TestBeam/PolarfireRawFile.cxx
+++ reformat/TestBeam/src/TestBeam/PolarfireRawFile.cxx
@@ -1,12 +1,16 @@
 
-#include <bitset>
 #include <iomanip>
 #include <optional>
+#include <tuple>
 
 #include "Reformat/RawDataFile.h"
 #include "Reformat/Utility/Hex.h"
 #include "Reformat/Utility/Mask.h"
 #include "Reformat/Utility/Reader.h"
+
+// uncomment to show decoding debug statements, 
+// only helpful if decoding of event header is suspect
+//#define SHOW_DECODING
 
 namespace testbeam {
 
@@ -15,14 +19,65 @@ class PolarfireRawFile : public reformat::RawDataFile {
   PolarfireRawFile(const framework::config::Parameters& p);
   virtual std::optional<reformat::EventPacket> next() final override;
  private:
+  /**
+   * Actual event reading function, we separate it from next so that
+   * we can implement skipping the first N spills
+   */
+  std::tuple<bool /*successful read*/,
+             uint32_t /*local spill*/,
+             uint32_t /*ticks*/,
+             reformat::EventPacket /*packet WITHOUT TIMESTAMP*/
+            > pop(); 
+ private:
   long int spill_{-1};
   long int i_spill_{0};
+  int spills_to_skip_{0};
 };
 
 PolarfireRawFile::PolarfireRawFile(const framework::config::Parameters& ps)
-  : RawDataFile(ps) {}
+  : RawDataFile(ps) {
+  spills_to_skip_ = ps.getParameter("spills_to_skip",spills_to_skip_);
+}
 
 std::optional<reformat::EventPacket> PolarfireRawFile::next() {
+  static bool keep_current_spill{spills_to_skip_<=0};
+  do {
+    // pop does the actual unpacking and decoding of the event header
+    auto [success, spill, ticks, ep] = pop();
+    reformat_log(debug) << "popped event at spill " << spill << " with ticks " << ticks;
+    // guard to return empty event packet upon end of file
+    if (not success) return {};
+    // check if new spill
+    if (spill != spill_) {
+      spill_ = spill;
+      if (spills_to_skip_ > 0) {
+        reformat_log(debug) << "Skipping spill " << spill;
+        --spills_to_skip_;
+        keep_current_spill = false;
+      } else {
+        reformat_log(debug) << "Keeping spill " << spill;
+        keep_current_spill = true;
+        ++i_spill_;
+      }
+    }
+
+    if (keep_current_spill) {
+      // not in skipping mode, return this event
+      reformat::EventPacket::TimestampType ts;
+      ts = i_spill_;
+      ts <<= 32;
+      ts += ticks;
+      reformat_log(debug) << "Timestamp(i_spill = " << i_spill_ 
+        << ", ticks = " << ticks << ") = " << ts;
+      ep.setTimestamp(ts);
+      return ep;
+    }
+  } while (not keep_current_spill);
+  ldmx_log(error) << "escaped parsing loop when we were supposed to keep the current spill";
+  return {};
+}
+
+std::tuple<bool,uint32_t, uint32_t, reformat::EventPacket> PolarfireRawFile::pop() {
   /// words for reading and decoding
   static uint32_t w;
 
@@ -30,6 +85,7 @@ std::optional<reformat::EventPacket> PolarfireRawFile::next() {
   w=0;
   while (file_reader_ and w != 0xbeef2021 and w != 0xbeef2022) {
     file_reader_ >> w;
+#ifdef SHOW_DECODING
     if (w == 0xbeef2021) {
       reformat_log(debug) << "Signal words imply version 1";
     } else if (w == 0xbeef2022) {
@@ -37,10 +93,14 @@ std::optional<reformat::EventPacket> PolarfireRawFile::next() {
     } else {
       reformat_log(debug) << "Extra header (inserted by rogue): " << reformat::utility::hex(w);
     }
+#endif
   }
-  if (!file_reader_ or file_reader_.eof()) return {};
 
   reformat::EventPacket ep;
+
+  if (!file_reader_ or file_reader_.eof()) 
+    return std::make_tuple(false,0,0,ep);
+
   ep.append(w);
 
   /* whole event header word looks like
@@ -68,6 +128,7 @@ std::optional<reformat::EventPacket> PolarfireRawFile::next() {
     EXCEPTION_RAISE("VersMis",
         "PolarfireRawFile only knows version 2 of DAQ format.");
   }
+#ifdef SHOW_DECODING
   reformat_log(debug) << reformat::utility::hex(w)
     << " EventHeader(version = " << version
     << ", fpga = " << fpga
@@ -75,6 +136,7 @@ std::optional<reformat::EventPacket> PolarfireRawFile::next() {
     << ", eventlen = " << eventlen
     << ")";
   reformat_log(debug) << "Sample Lengths: ";
+#endif
   // sample counters
   int n_words{0};
   std::vector<uint32_t> length_per_sample(nsamples, 0);
@@ -86,7 +148,9 @@ std::optional<reformat::EventPacket> PolarfireRawFile::next() {
     }
     uint32_t shift_in_word = 16*(i_sample%2);
     length_per_sample[i_sample] = (w >> shift_in_word) & reformat::utility::mask<12>;
+#ifdef SHOW_DECODING
     reformat_log(debug) << "len(" << i_sample << ") = " << length_per_sample[i_sample] << " ";
+#endif
   }
 
   /**
@@ -94,7 +158,9 @@ std::optional<reformat::EventPacket> PolarfireRawFile::next() {
    * firmware for DMA readout simpler. This means we readout the leftover
    * dummy words to move the pointer on the reader.
    */
+#ifdef SHOW_DECODING
   reformat_log(debug) << "Padding words to reach 8 total sample length words.";
+#endif
   for (int i_word{n_words}; i_word < 8; i_word++) {
     file_reader_ >> w; i_event++;
     ep.append(w);
@@ -107,20 +173,26 @@ std::optional<reformat::EventPacket> PolarfireRawFile::next() {
   ep.append(w);
   uint32_t spill = ((w >> 12) & 0xfff);
   uint32_t bunch = (w & 0xfff);
+#ifdef SHOW_DECODING
   reformat_log(debug) << " " << reformat::utility::hex(w) 
     << " Spill: " << spill 
     << " Bunch: " << bunch;
+#endif
   file_reader_ >> w; i_event++;
   ep.append(w);
   uint32_t ticks = w;
+#ifdef SHOW_DECODING
   reformat_log(debug) << " " << reformat::utility::hex(w) 
     << " 5 MHz Ticks since Spill: " << w
     << " Time: " << w/5e6 << "s";
+#endif
   file_reader_ >> w; i_event++;
   ep.append(w);
   uint32_t number = w;
+#ifdef SHOW_DECODING
   reformat_log(debug) << " " << reformat::utility::hex(w) 
     << " Event Number: " << w;
+#endif
   file_reader_ >> w; i_event++;
   ep.append(w);
   uint32_t run = (w & 0xFFF);
@@ -128,33 +200,20 @@ std::optional<reformat::EventPacket> PolarfireRawFile::next() {
   uint32_t MM = (w>>28)&0xF;
   uint32_t hh = (w>>18)&0x1F;
   uint32_t mm = (w>>12)&0x3F;
+#ifdef SHOW_DECODING
   reformat_log(debug) << " " << reformat::utility::hex(w) 
     << " Run: " << run << " DD-MM hh:mm "
     << DD << "-" << MM << " " << hh << ":" << mm;
-
-  /**
-   * Convert our event header information into global timestamp
-   */
-  if (spill != spill_) {
-    spill_ = spill;
-    i_spill_++;
-  }
-  reformat::EventPacket::TimestampType ts;
-  ts = i_spill_;
-  ts <<= 32;
-  ts += ticks;
-  reformat_log(debug) << " Timestamp(i_spill = " << i_spill_ << ", ticks = " << ticks << ") = " << ts;
-  ep.setTimestamp(ts);
-
+#endif
   std::vector<uint32_t> words;
   // i_event words from eventlen already been read above
   if (file_reader_.read(words, eventlen-i_event)) ep.append(words);
   else {
     reformat_log(error) << "File closed before event completed";
-    return {};
+    return std::make_tuple(false,spill,ticks,ep);
   }
-
-  return ep;
+  
+  return std::make_tuple(true,spill,ticks,ep);
 }
 
 }  // namespace testbeam
