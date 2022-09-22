759d7659ae063044177f8bfad84899d360b8d2f7
Erik Wallin <skelpdar@gmail.com>
2022-09-22 17:25:13 +0200

    TS decoding fixes in reformat

--- reformat/TestBeam/src/TestBeam/TrigScintEventGroupedRawFile.cxx
+++ reformat/TestBeam/src/TestBeam/TrigScintEventGroupedRawFile.cxx
@@ -38,21 +38,22 @@ std::optional<reformat::EventPacket> TrigScintEventGroupedRawFile::next() {
   // first 8 bytes are the two deprecated timestamps (UTC seconds and UTC clock ticks)
   static const std::size_t TIMESINCESPILL_POS = 8;
   // next 4 bytes are the time since spill
-  static const std::size_t TIMESINCESPILL_LEN_BYTES = 4;
-  uint32_t timeSpill=0;
-  for (int iW = 0; iW < TIMESINCESPILL_LEN_BYTES; iW++) {
-    int pos = TIMESINCESPILL_POS + iW;
+  static const std::size_t TIMESINCESPILL_LEN_BYTES = 3;
+  uint64_t timeSpill=0;
+  for (int iW = 0; iW <= TIMESINCESPILL_LEN_BYTES; iW++) {
+    //Convert from little endian to big
+    int pos = TIMESINCESPILL_POS + TIMESINCESPILL_LEN_BYTES - iW;
     timeSpill |= (buff.at(pos) << (TIMESINCESPILL_LEN_BYTES-iW)*8); //shift by a byte at a time
   }
   reformat_log(debug) << "time since spill " << timeSpill;
 
-  if (timeSpill < last_event_time) {
+  if (timeSpill < last_event_time && timeSpill != 0) {
     i_spill_++;
     reformat_log(debug) << "new spill " << i_spill_;
   }
   last_event_time = timeSpill;
 
-  uint64_t ts{(i_spill_ << 32)+timeSpill};
+  uint64_t ts{(((uint64_t)i_spill_) << 32)+timeSpill};
 
   ep.setTimestamp(ts);
 
